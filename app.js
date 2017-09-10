8fecb80c2b306736a85e9c10f1704d25cc96bbfe
pankaj-arunsingh <pankaj_arunsingh@ymail.com>
2017-09-10 15:42:22 +1000

    Setting up dialog configuration

--- app.js
+++ app.js
@@ -8,14 +8,43 @@ const bot= new builder.UniversalBot(
     [
         (session)=> {
            // session.send('Hello Pankaj! I am your bot!');
-           builder.Prompts.text(session,`Welcome to be amazed. Hello ! What is your name?`);
-        },
+           //builder.Prompts.text(session,`Welcome to be amazed. Hello ! What is your name?`);
+            session.beginDialog('ensureProfile',session.userData.profile);
+     },
            (session,results)=>{
-               session.endDialog(` Hello, ${results.response}`);
+               const profile=session.userData.profile=results.response;
+               session.endConversation(` Hello, ${profile.name}.
+               I Love ${profile.company}!`);
            }
     ]
 );
 
+bot.dialog('ensureProfile',[
+    (session,args,next)=>{
+        session.dialogData.profile=args||{};
+        if(!session.dialogData.profile.name){
+            builder.Prompts.text(session,`What's your name?`);
+        }else{
+            next();
+        }
+    },
+    (session,results,next)=>{
+        if(results.response){
+            session.dialogData.profile.name=results.response;
+        }
+        if(!session.dialogData.profile.company){
+            builder.Prompts.text(session,`What company do you work for?`);
+        }else{
+            next();
+        }
+    },
+    (session,results)=>{
+        if(results.response){
+            session.dialogData.profile.company=results.response;
+        }
+        session.endDialogWithResult({response:session.dialogData.profile})
+    }
+])
 // Go to this for sample bots and bot directory and docmenttaton 
 //https://bots.botsframework.com
 //creating web hosting server
